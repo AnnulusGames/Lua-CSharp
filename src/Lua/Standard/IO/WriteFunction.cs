476ed7e8bfc3a387fc4010084d753880f4f0186c
AnnulusGames <annulusgames@gmail.com>
2024-09-29 10:19:11 +0900

    Fix: io.write

--- src/Lua/Standard/IO/WriteFunction.cs
+++ src/Lua/Standard/IO/WriteFunction.cs
@@ -7,7 +7,7 @@ public sealed class WriteFunction : LuaFunction
 
     protected override ValueTask<int> InvokeAsyncCore(LuaFunctionExecutionContext context, Memory<LuaValue> buffer, CancellationToken cancellationToken)
     {
-        var file = context.State.Environment["io"].Read<LuaTable>()["stdio"].Read<FileHandle>();
+        var file = context.State.Environment["io"].Read<LuaTable>()["stdout"].Read<FileHandle>();
         var resultCount = IOHelper.Write(file, Name, context, buffer);
         return new(resultCount);
     }
