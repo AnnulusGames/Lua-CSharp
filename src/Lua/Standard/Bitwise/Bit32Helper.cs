568f3600e114441123fe9b1213ce152c52bbb58b
AnnulusGames <annulusgames@gmail.com>
2024-10-03 15:13:35 +0900

    Fix: Bit32Helper.ToUInt32

--- src/Lua/Standard/Bitwise/Bit32Helper.cs
+++ src/Lua/Standard/Bitwise/Bit32Helper.cs
@@ -17,8 +17,8 @@ internal static class Bit32Helper
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static uint ToUInt32(double d)
     {
-        d = Math.IEEERemainder(d, Math.Pow(2.0, 32.0));
-        return (uint)d;
+        var x = (int)Math.IEEERemainder(d, Math.Pow(2.0, 32.0));
+        return (uint)x;
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
