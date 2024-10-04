75e86bd66feebe56b1ba2ed5e830d773601f8fb0
AnnulusGames <annulusgames@gmail.com>
2024-10-04 21:32:00 +0900

    Fix: StringHelper.Slice

--- src/Lua/Standard/Text/StringHelper.cs
+++ src/Lua/Standard/Text/StringHelper.cs
@@ -14,11 +14,11 @@ internal static class StringHelper
     public static ReadOnlySpan<char> Slice(string s, int i, int j)
     {
         if (i < 0) i = s.Length + i + 1;
-        if (j < 0) i = s.Length + i + 1;
+        if (j < 0) j = s.Length + j + 1;
 
         if (i < 1) i = 1;
         if (j > s.Length) j = s.Length;
 
-        return i > j ? "" : s.AsSpan().Slice(i - 1, j - 1);
+        return i > j ? "" : s.AsSpan()[(i - 1)..j];
     }
 }
