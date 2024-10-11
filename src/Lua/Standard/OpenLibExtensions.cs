d97ef5ad4df6a91e1936dbecdf181ec87a0010c4
AnnulusGames <annulusgames@gmail.com>
2024-10-12 02:49:04 +0900

    Move: StringIndexMetamethod

--- src/Lua/Standard/OpenLibExtensions.cs
+++ src/Lua/Standard/OpenLibExtensions.cs
@@ -13,18 +13,6 @@ namespace Lua.Standard;
 
 public static class OpenLibExtensions
 {
-    sealed class StringIndexMetamethod(LuaTable table) : LuaFunction
-    {
-        protected override ValueTask<int> InvokeAsyncCore(LuaFunctionExecutionContext context, Memory<LuaValue> buffer, CancellationToken cancellationToken)
-        {
-            context.GetArgument<string>(0);
-            var key = context.GetArgument(1);
-
-            buffer.Span[0] = table[key];
-            return new(1);
-        }
-    }
-
     static readonly LuaFunction[] baseFunctions = [
         AssertFunction.Instance,
         ErrorFunction.Instance,
