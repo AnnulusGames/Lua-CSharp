af49ee766355b961f932f7111d72dd1092ca60e5
AnnulusGames <annulusgames@gmail.com>
2024-10-17 21:14:33 +0900

    Fix: error id

--- src/Lua.SourceGenerator/DiagnosticDescriptors.cs
+++ src/Lua.SourceGenerator/DiagnosticDescriptors.cs
@@ -23,7 +23,7 @@ public static class DiagnosticDescriptors
         isEnabledByDefault: true);
 
     public static readonly DiagnosticDescriptor AbstractNotAllowed = new(
-        id: "LUAC003",
+        id: "LUACS003",
         title: "LuaObject type must not abstract",
         messageFormat: "LuaObject object '{0}' must be not abstract",
         category: Category,
@@ -31,7 +31,7 @@ public static class DiagnosticDescriptors
         isEnabledByDefault: true);
 
     public static readonly DiagnosticDescriptor InvalidPropertyType = new(
-        id: "LUAC004",
+        id: "LUACS004",
         title: "The type of the field or property must be LuaValue or a type that can be converted to LuaValue.",
         messageFormat: "The type of '{0}' must be LuaValue or a type that can be converted to LuaValue.",
         category: Category,
@@ -39,7 +39,7 @@ public static class DiagnosticDescriptors
         isEnabledByDefault: true);
 
     public static readonly DiagnosticDescriptor InvalidReturnType = new(
-        id: "LUAC005",
+        id: "LUACS005",
         title: "The return type must be LuaValue or types that can be converted to LuaValue.",
         messageFormat: "The return type '{0}' must be LuaValue or types that can be converted to LuaValue.",
         category: Category,
@@ -47,7 +47,7 @@ public static class DiagnosticDescriptors
         isEnabledByDefault: true);
 
     public static readonly DiagnosticDescriptor InvalidParameterType = new(
-        id: "LUAC006",
+        id: "LUACS006",
         title: "The parameters must be LuaValue or types that can be converted to LuaValue.",
         messageFormat: "The parameter '{0}' must be LuaValue or types that can be converted to LuaValue.",
         category: Category,
@@ -55,7 +55,7 @@ public static class DiagnosticDescriptors
         isEnabledByDefault: true);
 
     public static readonly DiagnosticDescriptor DuplicateMetamethod = new(
-        id: "LUAC007",
+        id: "LUACS007",
         title: "The type already contains same metamethod.",
         messageFormat: "Type '{0}' already contains a '{1}' metamethod.,",
         category: Category,
