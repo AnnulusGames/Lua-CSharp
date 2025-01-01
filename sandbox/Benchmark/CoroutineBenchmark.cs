340dcd9aca49e67b18425781a821bc6ef511a3ba
Akeit0 <90429982+Akeit0@users.noreply.github.com>
2025-01-01 17:43:10 +0900

    Fix: CoroutineBenchmark fails

--- sandbox/Benchmark/CoroutineBenchmark.cs
+++ sandbox/Benchmark/CoroutineBenchmark.cs
@@ -14,6 +14,7 @@ public class CoroutineBenchmark
     {
         core.Setup("coroutine.lua");
         core.LuaCSharpState.OpenBasicLibrary();
+        core.LuaCSharpState.OpenCoroutineLibrary();
     }
 
     [Benchmark(Description = "MoonSharp (RunString)")]
